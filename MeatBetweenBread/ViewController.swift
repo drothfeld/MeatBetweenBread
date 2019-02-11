dba31c1b384d5c58c8fe1c9791cebf9853e5bacf
Dylan Rothfeld <drothfel@gmail.com>
2019-02-10 18:32:57 -0800

    created all storyboard outlets

--- MeatBetweenBread/ViewController.swift
+++ MeatBetweenBread/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import ImageIO
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var ClassificationLabel: UILabel!
+    @IBOutlet weak var Toolbar: UIToolbar!
+    @IBOutlet weak var SelectImageToolbarButton: UIBarButtonItem!
+    @IBOutlet weak var SaveImageToolbarButton: UIBarButtonItem!
+    @IBOutlet weak var OpenCameraButton: UIButton!
+    @IBOutlet weak var UntouchedMessageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func CameraButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func SelectImageButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func SaveImageButtonPressed(_ sender: Any) {
+    }
+    
 }
 
