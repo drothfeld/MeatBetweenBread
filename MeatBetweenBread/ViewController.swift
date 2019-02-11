c14fdf641ff0641cefe87b929222875fb43913b3
Dylan Rothfeld <drothfel@gmail.com>
2019-02-10 18:49:03 -0800

    finished photo functions and tested on actual device

--- MeatBetweenBread/ViewController.swift
+++ MeatBetweenBread/ViewController.swift
@@ -69,18 +69,91 @@ class ViewController: UIViewController {
         }
     }
     
+    func updateClassification(for image: UIImage) {
+        ClassificationLabel.text = "Processing..."
+        ClassificationLabel.isHidden = false
+        UntouchedMessageLabel.isHidden = true
+        SaveImageToolbarButton.isEnabled = true
+        
+        // Attempt to create CIImage for processing
+        let orientation = CGImagePropertyOrientation(image.imageOrientation)
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("Unable to create \(CIImage.self) from \(image).")
+        }
+        
+        // Make a classification request to process the image
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
+            do {
+                try handler.perform([self.classificationRequest])
+            }
+                
+                // Handler that attempts to catch general image processing errors
+            catch {
+                print("Failed to perform classification. \n\(error.localizedDescription)")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func CameraButtonPressed(_ sender: Any) {
+        // If the camera view is not available, show the photolibrary instead
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            presentPhotoPicker(sourceType: .photoLibrary)
+            return
+        }
+        
+        self.presentPhotoPicker(sourceType: .camera)
     }
     
     @IBAction func SelectImageButtonPressed(_ sender: Any) {
+        self.presentPhotoPicker(sourceType: .photoLibrary)
     }
     
     @IBAction func SaveImageButtonPressed(_ sender: Any) {
+        UIImageWriteToSavedPhotosAlbum(ImageView.image!, self, #selector(saveImage(_:didFinishSavingWithError:contextInfo:)), nil)
+        SaveImageToolbarButton.isEnabled = false
     }
     
+    func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true)
+    }
+    
+    @objc func saveImage(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        // Error when trying to save image
+        if let error = error {
+            let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: .default))
+            present(alertController, animated: true)
+            
+            // Successfully saved image
+        } else {
+            let alertController = UIAlertController(title: "Saved", message: "The image has been saved to your photos.", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: .default))
+            present(alertController, animated: true)
+        }
+    }
+    
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // Handles image picker selection
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Handle the chosen image from the UIImage picker
+        picker.dismiss(animated: true)
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        ImageView.image = selectedImage
+        updateClassification(for: selectedImage)
+    }
 }
 
